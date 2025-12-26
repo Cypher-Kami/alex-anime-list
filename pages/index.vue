7560fb473f92bcd58a826c447c5e42aa774845cf
Alex Bitar Méndez <xelbitar@gmail.com>
2025-12-26 21:04:00 +0100

    testing tailwnd and daisyui

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+console.log('helloooooww')
+</script>
+
+<template>
+  <div class="flex min-h-screen flex-col">
+    <main class="flex-1 flex flex-col">
+      helllooooo
+    </main>
+  </div>
+</template>
